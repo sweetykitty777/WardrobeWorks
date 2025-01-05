92a3aacbd706ae22b83ed03076c55a935910fed6
O <olya.shavochkina@yandex.ru>
2025-01-05 17:04:42 +0300

    Initial Commit

---
+++ diploma/diplomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  diplomaApp.swift
+//  diploma
+//
+//  Created by Olga on 05.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct diplomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
